e485365fa6f3b454278ed42e463c82f6fe2938b7
root <AarioAi@gmail.com>
2024-12-05 11:01:36 +0000

    refactor:return context to httpsvr.New

--- core/httpsvr/base.go
+++ core/httpsvr/base.go
@@ -1,14 +1,15 @@
 package httpsvr
 
 import (
+	"context"
 	"github.com/aarioai/airis/core/httpsvr/request"
 	"github.com/aarioai/airis/core/httpsvr/response"
 	"github.com/kataras/iris/v12"
 )
 
 // 读取json buffer 的时候，会清空掉 r.Body，所以这个使用一次；
-func New(ictx iris.Context) (*request.Request, *response.Writer) {
+func New(ictx iris.Context) (*request.Request, *response.Writer, context.Context) {
 	r := request.New(ictx)
 	w := response.NewWriter(ictx, r)
-	return r, w
+	return r, w, r.Context()
 }
