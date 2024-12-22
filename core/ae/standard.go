5c97b6a293343af23ea6793ce8425a6ff8a9eb3d
root <AarioAi@gmail.com>
2024-12-22 19:26:57 +0000

    feat:add some ae input errors

--- core/ae/standard.go
+++ core/ae/standard.go
@@ -15,21 +15,36 @@ var (
 	ErrServiceUnavailable = errors.New("service unavailable")
 	ErrDeprecated         = errors.New("deprecated") // 已废弃的功能
 
-	ErrEmptyInput       = errors.New("input is empty") // 必需的参数未传递，或者传递为空
-	ErrInputWrongLength = errors.New("input wrong length")
 	ErrInvalidInput     = errors.New("invalid input") // 存在无效的参数
-
+	ErrInputWrongLength = errors.New("input wrong length")
+	ErrEmptyInput       = errors.New("input is empty") // 必需的参数未传递，或者传递为空
+	ErrInputTooLong     = errors.New("input is too long")
+	ErrInputTooShort    = errors.New("input is too short")
+	ErrInputTooBig      = errors.New("input is too big")
+	ErrInputTooSmall    = errors.New("input is too small")
 )
 
 // *Error
 // ErrorXXX/ErrXXX  都应被视为常量，不应修改
 var (
-	ErrorEmptyInput       = NewError(ErrEmptyInput).WithCaller(2).Lock()
+	ErrorDeprecated = NewError(ErrDeprecated).WithCaller(2).Lock()
+
 	ErrorInvalidInput     = NewError(ErrInvalidInput).WithCaller(2).Lock()
 	ErrorInputWrongLength = NewError(ErrInputWrongLength).WithCaller(2).Lock()
-	ErrorDeprecated       = NewError(ErrDeprecated).WithCaller(2).Lock()
+	ErrorEmptyInput       = NewError(ErrEmptyInput).WithCaller(2).Lock()
+	ErrorInputTooLong     = NewError(ErrInputTooLong).WithCaller(2).Lock()
+	ErrorInputTooShort    = NewError(ErrInputTooShort).WithCaller(2).Lock()
+	ErrorInputTooBig      = NewError(ErrInputTooBig).WithCaller(2).Lock()
+	ErrorInputTooSmall    = NewError(ErrInputTooSmall).WithCaller(2).Lock()
 )
 
+func ErrInvalid(value any, name ...string) error {
+	return errors.New("invalid input  " + packArg(value, name))
+}
+
+func ErrorInvalid(value any, name ...string) *Error {
+	return NewE("invalid input " + packArg(value, name))
+}
 func packArg(value any, name []string) string {
 	var s string
 	if len(name) > 0 {
@@ -39,11 +54,4 @@ func packArg(value any, name []string) string {
 		s += fmt.Sprintf(": %v", value)
 	}
 	return s
-}
-func ErrInvalid(value any, name ...string) error {
-	return errors.New("invalid input  " + packArg(value, name))
-}
-
-func ErrorInvalid(value any, name ...string) *Error {
-	return NewE("invalid input " + packArg(value, name))
 }
