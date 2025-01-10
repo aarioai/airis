d4c5c9738246229bfe93652d0f553b1e162c2eaf
Aario <AarioAi@gmail.com>
2025-01-10 15:13:50 +0800

    pipefail

--- core/ae/util_check.go
+++ core/ae/util_check.go
@@ -13,24 +13,28 @@ func FirstErr(errs ...error) error {
 	return afmt.First(errs)
 }
 
-func Panic(format string, args ...any) {
+func PanicWithCaller(callerSkip int, format string, args ...any) {
 	now := time.Now().Format("2006-01-02 15:04:05")
-	msg := now + " [panic] " + fmt.Sprintf(format, args...)
+	msg := now + " [panic] " + Caller(callerSkip) + " " + fmt.Sprintf(format, args...)
 	fmt.Println(msg) // convenient for docker/local debugging
 	panic(msg)
+}
+
+func Panic(format string, args ...any) {
+	PanicWithCaller(2, format, args...)
 }
 
 // PanicOn 如果存在服务器错误则触发 panic
 func PanicOn(es ...*Error) {
 	if e := First(es...); e != nil {
-		Panic(e.Text())
+		PanicWithCaller(2, e.Text())
 	}
 }
 
 // PanicOnErrors 断言检查标准错误，如果存在错误则触发 panic
 func PanicOnErrs(errs ...error) {
 	if e := FirstErr(errs...); e != nil {
-		Panic(e.Error())
+		PanicWithCaller(2, e.Error())
 	}
 }
 func StopOnFirstError(callables ...func() *Error) *Error {
