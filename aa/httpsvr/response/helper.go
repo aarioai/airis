6edd1e3338e6ebcf93335738382fe3bd516cafcb
Aario <AarioAi@gmail.com>
2025-04-14 17:51:56 +0800

    feat:response helper isOK and OK

--- aa/httpsvr/response/helper.go
+++ aa/httpsvr/response/helper.go
@@ -12,3 +12,20 @@ func (w *Writer) FirstError(es ...*ae.Error) *ae.Error {
 	}
 	return nil
 }
+
+func (w *Writer) IsOK(catchResponded bool, e *ae.Error) bool {
+	if catchResponded {
+		return false
+	}
+	if e != nil {
+		w.WriteE(e)
+		return false
+	}
+	return true
+}
+
+func (w *Writer) OK(catchResponded bool, e *ae.Error) {
+	if ok := w.IsOK(catchResponded, e); ok {
+		w.WriteOK()
+	}
+}
