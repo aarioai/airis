abfdb71b45b407703ba6bae570bbb8be940f46db
Aario <AarioAi@gmail.com>
2025-04-14 17:39:04 +0800

    chore:gitignore

--- cmd/f_oss_filetype_readonly.js
+++ cmd/f_oss_filetype_readonly.js
@@ -1,6 +1,6 @@
 /** @note this is an auto-generated file, do not modify it! */
 
-/** @typedef {".zip"|"application/zip"|"application/x-zip-compressed"|"multipart/x-zip"|".rar"|"application/vnd.rar"|"application/x-rar-compressed"|".bz"|"application/x-bzip"|".bz2"|"application/x-bzip2"|".gz"|"application/gzip"|"application/x-gzip"|".json"|"application/json"|".ico"|"image/vnd.microsoft.icon"|"image/x-icon"|".svg"|"image/svg+xml"|".jpg"|"image/jpeg"|".jpeg"|".png"|"image/png"|".gif"|"image/gif"|".webp"|"image/webp"|".heic"|"image/heic"|".heif"|".avci"|"image/heif"|".3gp"|"audio/3gpp"|".3g2"|"audio/3gpp2"|".aiff"|"audio/aiff"|".aif"|".aifc"|"audio/x-aiff"|".wav"|"audio/wav"|".webm"|"audio/webm"|".mp3"|"audio/mpeg"|"audio/mp3"|"video/x-wav"|".avi"|"video/x-msvideo"|".mov"|"video/quicktime"|".mpeg"|"video/mpeg"|".mp4"|"video/mp4"|"video/3gpp"|"video/3gpp2"|"video/webm"|".doc"|"application/msword"|".docx"|"application/vnd.openxmlformats-officedocument.wordprocessingml.document"|".xls"|"application/vnd.ms-excel"|".xlsx"|"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"|".ppt"|"application/vnd.ms-powerpoint"|".pdf"|"application/pdf"|".txt"|"text/plain"|".md"|"text/markdown"|".pptx"|"application/vnd.openxmlformats-officedocument.presentationml.presentation"} AaFileTypeMime */
+/** @typedef {".zip"|"application/zip"|"application/x-zip-compressed"|"multipart/x-zip"|".rar"|"application/vnd.rar"|"application/x-rar-compressed"|".bz"|"application/x-bzip"|".bz2"|"application/x-bzip2"|".gz"|"application/gzip"|"application/x-gzip"|".json"|"application/json"|".gif"|"image/gif"|".webp"|"image/webp"|".heic"|"image/heic"|".heif"|".avci"|"image/heif"|".ico"|"image/vnd.microsoft.icon"|"image/x-icon"|".svg"|"image/svg+xml"|".jpg"|"image/jpeg"|".jpeg"|".png"|"image/png"|".3g2"|"audio/3gpp2"|".aiff"|"audio/aiff"|".aif"|".aifc"|"audio/x-aiff"|".wav"|"audio/wav"|".webm"|"audio/webm"|".mp3"|"audio/mpeg"|"audio/mp3"|".3gp"|"audio/3gpp"|".mpeg"|"video/mpeg"|".mp4"|"video/mp4"|"video/3gpp"|"video/3gpp2"|"video/webm"|"video/x-wav"|".avi"|"video/x-msvideo"|".mov"|"video/quicktime"|".txt"|"text/plain"|".md"|"text/markdown"|".doc"|"application/msword"|".docx"|"application/vnd.openxmlformats-officedocument.wordprocessingml.document"|".pdf"|"application/pdf"|".xls"|"application/vnd.ms-excel"|".xlsx"|"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"|".ppt"|"application/vnd.ms-powerpoint"|".pptx"|"application/vnd.openxmlformats-officedocument.presentationml.presentation"} AaFileTypeMime */
 
 class AaFileType {
     /** @enum */
@@ -44,6 +44,13 @@ class AaFileType {
         Json        : 10000,
     }
     static Mimes = {
+        Compressed : {
+            Bzip        : [".bz", "application/x-bzip"],
+            Bzip2       : [".bz2", "application/x-bzip2"],
+            Gzip        : [".gz", "application/gzip", "application/x-gzip"],
+            Rar         : [".rar", "application/vnd.rar", "application/x-rar-compressed"],
+            Zip         : [".zip", "application/zip", "application/x-zip-compressed", "multipart/x-zip"],
+        },
         Data : {
             Json        : [".json", "application/json"],
         },
@@ -84,13 +91,6 @@ class AaFileType {
             Txt         : [".txt", "text/plain"],
             Xls         : [".xls", "application/vnd.ms-excel"],
             Xlsx        : [".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"],
-        },
-        Compressed : {
-            Bzip        : [".bz", "application/x-bzip"],
-            Bzip2       : [".bz2", "application/x-bzip2"],
-            Gzip        : [".gz", "application/gzip", "application/x-gzip"],
-            Rar         : [".rar", "application/vnd.rar", "application/x-rar-compressed"],
-            Zip         : [".zip", "application/zip", "application/x-zip-compressed", "multipart/x-zip"],
         },
     }
     contentType
