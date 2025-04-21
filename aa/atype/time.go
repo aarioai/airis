ac90e99f9ec9c037964907b27aa20f532d9f4002
Aario <AarioAi@gmail.com>
2025-04-21 15:23:05 +0800

    feat:atype.DurationSeconds

--- aa/atype/time.go
+++ aa/atype/time.go
@@ -1,1 +1,13 @@
 package atype
+
+import "time"
+
+type DurationSeconds int64
+
+func ToDurationSeconds(t time.Duration) DurationSeconds {
+	return DurationSeconds(t.Seconds())
+}
+
+func (d DurationSeconds) Duration() time.Duration {
+	return time.Duration(d) * time.Second
+}
--- cmd/f_oss_filetype_readonly.js
+++ cmd/f_oss_filetype_readonly.js
@@ -1,6 +1,6 @@
 /** @note this is an auto-generated file, do not modify it! */
 
-/** @typedef {".3g2"|"audio/3gpp2"|".aiff"|"audio/aiff"|".aif"|".aifc"|"audio/x-aiff"|".wav"|"audio/wav"|".webm"|"audio/webm"|".mp3"|"audio/mpeg"|"audio/mp3"|".3gp"|"audio/3gpp"|"video/3gpp2"|"video/webm"|"video/x-wav"|".avi"|"video/x-msvideo"|".mov"|"video/quicktime"|".mpeg"|"video/mpeg"|".mp4"|"video/mp4"|"video/3gpp"|".docx"|"application/vnd.openxmlformats-officedocument.wordprocessingml.document"|".xls"|"application/vnd.ms-excel"|".xlsx"|"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"|".ppt"|"application/vnd.ms-powerpoint"|".pdf"|"application/pdf"|".txt"|"text/plain"|".md"|"text/markdown"|".doc"|"application/msword"|".pptx"|"application/vnd.openxmlformats-officedocument.presentationml.presentation"|".zip"|"application/zip"|"application/x-zip-compressed"|"multipart/x-zip"|".rar"|"application/vnd.rar"|"application/x-rar-compressed"|".bz"|"application/x-bzip"|".bz2"|"application/x-bzip2"|".gz"|"application/gzip"|"application/x-gzip"|".json"|"application/json"|".jpg"|"image/jpeg"|".jpeg"|".png"|"image/png"|".gif"|"image/gif"|".webp"|"image/webp"|".heic"|"image/heic"|".heif"|".avci"|"image/heif"|".ico"|"image/vnd.microsoft.icon"|"image/x-icon"|".svg"|"image/svg+xml"} AaFileTypeMime */
+/** @typedef {".json"|"application/json"|".jpg"|"image/jpeg"|".jpeg"|".png"|"image/png"|".gif"|"image/gif"|".webp"|"image/webp"|".heic"|"image/heic"|".heif"|".avci"|"image/heif"|".ico"|"image/vnd.microsoft.icon"|"image/x-icon"|".svg"|"image/svg+xml"|".mp3"|"audio/mpeg"|"audio/mp3"|".3gp"|"audio/3gpp"|".3g2"|"audio/3gpp2"|".aiff"|"audio/aiff"|".aif"|".aifc"|"audio/x-aiff"|".wav"|"audio/wav"|".webm"|"audio/webm"|".mov"|"video/quicktime"|".mpeg"|"video/mpeg"|".mp4"|"video/mp4"|"video/3gpp"|"video/3gpp2"|"video/webm"|"video/x-wav"|".avi"|"video/x-msvideo"|".xlsx"|"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"|".xls"|"application/vnd.ms-excel"|".ppt"|"application/vnd.ms-powerpoint"|".pptx"|"application/vnd.openxmlformats-officedocument.presentationml.presentation"|".pdf"|"application/pdf"|".txt"|"text/plain"|".md"|"text/markdown"|".doc"|"application/msword"|".docx"|"application/vnd.openxmlformats-officedocument.wordprocessingml.document"|".bz"|"application/x-bzip"|".bz2"|"application/x-bzip2"|".gz"|"application/gzip"|"application/x-gzip"|".zip"|"application/zip"|"application/x-zip-compressed"|"multipart/x-zip"|".rar"|"application/vnd.rar"|"application/x-rar-compressed"} AaFileTypeMime */
 
 class AaFileType {
     /** @enum */
@@ -44,6 +44,9 @@ class AaFileType {
         Json        : 10000,
     }
     static Mimes = {
+        Data : {
+            Json        : [".json", "application/json"],
+        },
         Image : {
             Gif         : [".gif", "image/gif"],
             Heic        : [".heic", "image/heic", ".heif", ".avci", "image/heif"],
@@ -89,9 +92,6 @@ class AaFileType {
             Rar         : [".rar", "application/vnd.rar", "application/x-rar-compressed"],
             Zip         : [".zip", "application/zip", "application/x-zip-compressed", "multipart/x-zip"],
         },
-        Data : {
-            Json        : [".json", "application/json"],
-        },
     }
     contentType
     ext
@@ -115,12 +115,12 @@ class AaFileType {
 			}
 		}
     }
-    isCompressed(){return this.mimeType === "Compressed"}
     isData(){return this.mimeType === "Data"}
     isImage(){return this.mimeType === "Image"}
     isAudio(){return this.mimeType === "Audio"}
     isVideo(){return this.mimeType === "Video"}
     isDocument(){return this.mimeType === "Document"}
+    isCompressed(){return this.mimeType === "Compressed"}
     toJSON(){return this.value}
     valueOf(){return this.value}
 }
