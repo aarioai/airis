ab2b844ca400493be0681959673ddb4be2967668
Aario <AarioAi@gmail.com>
2025-06-05 05:17:05 +0800

    feat(pkg/arrmap):PanicIfNotBase

--- pkg/arrmap/validate.go
+++ pkg/arrmap/validate.go
@@ -1,5 +1,7 @@
 package arrmap
 
+import "fmt"
+
 // IsBase checks if the digits slice has the specified length and contains no duplicate values
 // Example:
 // IsBase(36, []byte("0123456789abcdefghijklmnopqrstuvwxyz"))   // check is base36 base bytes
@@ -8,4 +10,10 @@ func IsBase(base int, digits []byte) bool {
 		return false
 	}
 	return !HasDuplicates(digits)
+}
+
+func PanicIfNotBase(base int, digits []byte) {
+	if !IsBase(base, digits) {
+		panic(fmt.Sprintf("%s is not the base bytes of base%d", string(digits), base))
+	}
 }
